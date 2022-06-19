803a911688a19c568eb69c95f86323d8b975bb65
Chris <sevenwonders.info@gmail.com>
2022-06-19 16:08:24 +0300

    Services for the corresponding operations defined in controllers

---
+++ src/main/java/com/example/battleships/services/AuthenticationService.java
@@ -0,0 +1,11 @@
+package com.example.battleships.services;
+
+import com.example.battleships.models.DTOs.UserLoginDTO;
+import com.example.battleships.models.DTOs.UserRegisterDTO;
+
+public interface AuthenticationService {
+
+    boolean register(UserRegisterDTO userRegisterDTO);
+
+    boolean login(UserLoginDTO loginDTO);
+}
