ab2f9b95ba810a30082bb4a6c255cf2077ee1e19
Chris <sevenwonders.info@gmail.com>
2022-06-21 18:14:21 +0300

    Controller methods for ship addition

---
+++ src/main/java/com/example/battleships/web/ShipController.java
@@ -0,0 +1,39 @@
+package com.example.battleships.web;
+
+import com.example.battleships.models.DTOs.CreateShipDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+public class ShipController {
+
+    @ModelAttribute("createShipDTO")
+    public CreateShipDTO initCreateShipDTO() {
+        return new CreateShipDTO();
+    }
+
+    @GetMapping("/ships/add")
+    public String addShip() {
+        return "ship-add";
+    }
+
+    @PostMapping("/ships/add")
+    public String ships(@Valid CreateShipDTO createShipDTO, BindingResult bindingResult,
+                        RedirectAttributes redAtrs) {
+
+        if (bindingResult.hasErrors()) {
+            redAtrs.addFlashAttribute("createShipDTO", createShipDTO);
+            redAtrs.addFlashAttribute
+                    ("com.springframework.validation.BindingResult.createShipDTO",
+                            bindingResult);
+            return "redirect:/ships/add";
+        }
+        return "redirect:/home";
+    }
+}
